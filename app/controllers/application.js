3082a3af62437d37fee97b443dbf260d2d05c507
Martin Skowronski <skowronski.martin@gmail.com>
2015-09-16 20:04:48 +0200

    added bootstrap

---
+++ app/controllers/application.js
@@ -0,0 +1,39 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  /*
+  currentTab: null,
+  availableTabs: ["overview", "assignments", "members"],
+
+  initialize: Ember.on('init', function() {
+    set(this, 'currentTab', 'overview');
+  }),
+
+  overviewActive: Ember.computed('currentTab', {
+    get: function() {
+      return get(this, 'currentTab') === "overview";
+    }
+  }),
+
+  assignmentsActive: Ember.computed('currentTab', {
+    get: function() {
+      return get(this, 'currentTab') === "assignments";
+    }
+  }),
+
+  membersActive: Ember.computed('currentTab', {
+    get: function() {
+      return get(this, 'currentTab') === "members";
+    }
+  }),
+
+
+  actions: {
+    select: function(tabname) {
+      if (get(this, 'availableTabs').indexOf(tabname) >= 0) {
+        set(this, 'currentTab', tabname);
+      }
+    },
+  }
+  */
+});
