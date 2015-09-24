330cf5876080a768a86e268abf0c568aea3adf81
Martin Skowronski <skowronski.martin@gmail.com>
2015-09-25 00:30:36 +0200

    refactored layout

--- app/controllers/overview.js
+++ app/controllers/overview.js
@@ -5,10 +5,26 @@ export default Ember.Controller.extend({
   eventManager: Ember.inject.service('events'),
   showBreadPath: true,
   currentUnit: null,
+  content: null,
 
   setup: Ember.on('init', function() {
+//     var org = null;
+//     var data = this.get('content');
+//     console.debug("DATA>", data);
+    /*
+    for (var i = 0; i < data.get('length') && !org; i++) {
+      var el = data.objectAt(i);
+      if (el.get('type') === "org") {
+        org = el;
+      }
+    };
+*/
     this.get('eventManager').on('setDetails', this.setDetails.bind(this));
   }),
+
+  duration: function() {
+    console.debug("ciontent changed");
+  }.property('content'),
 
   setDetails: function(unitId) {
     if (unitId !== undefined) {
--- app/routes/overview.js
+++ app/routes/overview.js
@@ -5,6 +5,11 @@ export default Ember.Route.extend({
     this.controllerFor('application').set('showBreadPath', true);
   },
   model: function() {
+    console.debug("RETURN ALL>");
     return this.store.findAll('unit');
   },
+  setupController: function(controller, model) {
+    console.debug("SET UP CONTROLLER");
+    controller.set('content', model);
+  }
 });
