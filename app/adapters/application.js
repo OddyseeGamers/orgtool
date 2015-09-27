8cf10a4b51b508f6b1e3351645065b1f537677b9
Martin Skowronski <skowronski.martin@gmail.com>
2015-09-27 02:12:11 +0200

    fixed depreceation warnings

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,15 @@ import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.extend({
 // export default DS.JSONAPIAdapter.extend({
-  namespace: 'api'
+  namespace: 'api',
+
+
+  shouldBackgroundReloadRecord: function() {
+    return true;
+  },
+
+  shouldReloadAll: function() {
+    return true;
+  },
 });
 
