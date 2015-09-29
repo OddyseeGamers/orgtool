6eca828c42885cb557280e8f9221cd4e58b5b758
Martin Skowronski <skowronski.martin@gmail.com>
2015-09-29 22:10:40 +0200

    added drop-hover style

--- app/components/member-details.js
+++ app/components/member-details.js
@@ -18,6 +18,7 @@ export default Ember.Component.extend({
       tolerance: 'pointer',
       helper: 'clone',
       cursorAt: { left: -5, top: -5 }, 
+      zIndex: 10,
       revert: true,
       drag: Ember.$.proxy(this.onDrag, this),
       stop: Ember.$.proxy(this.onDropped, this),
@@ -57,25 +58,41 @@ export default Ember.Component.extend({
     var last = this.get('lastElement');
     if (last) {
       $(last).css({ fill: this.get('lastColor')});
+
+      var classes = $(last).attr("class");
+      if (classes) {
+        classes = classes.split(" ");
+        var idx = classes.indexOf("drop-hover");
+        if (idx >= 0) {
+          console.debug("before", classes);
+          classes.splice(idx, 1);
+          console.debug("after", classes);
+          $(last).attr("class", classes.join(" "));
+        }
+      }
     }
     this.set('lastElement', null);
     this.set('lastColor', null);
   },
 
   setLast: function(element) {
-    this.resetLast();
     var last = this.get('lastElement');
-    if (last) {
-      $(last).css({ fill: this.get('lastColor')});
+    if (last === element) {
+      return;
     }
-
+    this.resetLast();
     this.set('lastElement', element);
     this.set('lastColor', $(element).css('fill'));
-    $(element).css({ fill: "#424242"});
-  },
-
-  draggedMatchesTarget: function(item) {
-    return this.getElementId(item).unitid !== undefined;
+    $(element).css("fill", "");
+    var classes = $(element).attr("class");
+    console.debug(">>> classes", classes);
+    if (classes) {
+      classes = classes.split(" ");
+    } else {
+      classes = [];
+    }
+    classes.push("drop-hover");
+    $(element).attr("class", classes.join(" "));
   },
 
   getElementId: function(item) {
