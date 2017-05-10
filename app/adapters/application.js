7ca97473aae51e5f997afd9e2dd4ea12168319b2
Martin Skowronski <skowronski.martin@gmail.com>
2017-05-10 15:14:35 +0200

    added query parameters for recrusion

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -10,6 +10,54 @@ export default DS.JSONAPIAdapter.extend({
   host: config.APP.API_HOST,
   namespace: 'api',
 
+  queryRecord(store, type, query) {
+//     Ember.Logger.log("QUERY", store, "-------", type.modelName, "------------", query);
+
+    if (query.id && query.recursive && type.modelName == "unit") {
+      let url = this.buildURL(type.modelName, null, null, 'queryRecord', null);
+      url += "/" + query.id + "?recursive=" + query.recursive;
+//       let url = this.buildURL(type.modelName, query.id, null, 'queryRecord', query);
+//       let url = this.namespace + "/" + Ember.String.pluralize(type.modelName) + "/" + query.id + "?recursive=" + query.recursive;
+//       delete query.id
+      console.log(">>>> URL", url);
+
+      return this.ajax(url, 'GET', {});
+//       return this.ajax(url, 'GET', { data: query });
+//       return Ember.$.getJSON(url);
+    } else {
+      let url = this.buildURL(type.modelName, null, null, 'queryRecord', query);
+
+      if (this.sortQueryParams) {
+        query = this.sortQueryParams(query);
+      }
+
+      return this.ajax(url, 'GET', { data: query });
+    }
+  },
+
+  query(store, type, query) {
+    if (query.id && query.recursive && type.modelName == "unit") {
+      let url = this.buildURL(type.modelName, null, null, 'query', null);
+      url += "/" + query.id + "?recursive=" + query.recursive;
+      console.log(">>>> URL", url);
+
+      return this.ajax(url, 'GET', {});
+    } else {
+      let url = this.buildURL(type.modelName, null, null, 'query', query);
+
+      if (this.sortQueryParams) {
+        query = this.sortQueryParams(query);
+      }
+
+      return this.ajax(url, 'GET', { data: query });
+    }
+  },
+
+//   pathForType: function(type) {
+//     Ember.Logger.log("---> type", type);
+//     return Ember.String.underscore(type);
+//   }
+
 /*
 modelNameFromPayloadKey: function(key) {
     Ember.Logger.log("---> model", key);
