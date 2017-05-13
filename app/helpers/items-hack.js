b83d9a0a9948637fe5bb7410502f5c8c2a72ed72
Martin Skowronski <skowronski.martin@gmail.com>
2017-05-13 16:31:20 +0200

    fixed item details image

--- app/helpers/items-hack.js
+++ app/helpers/items-hack.js
@@ -4,8 +4,11 @@ var get = Ember.get;
 var set = Ember.set;
 
 export function itemsHack(params/*, hash*/) {
-  if (params.length == 1) {
-    params[0].reload();
+  if (params.length == 1 && !Ember.isEmpty(params[0])) {
+    var typename = get(params[0], 'constructor.modelName');
+    if (typename == "item" || typename == "player") {
+      params[0].reload();
+    }
   }
 }
 
