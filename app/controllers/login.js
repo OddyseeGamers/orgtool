b4de00bf2f0e5bb6e77e8c95b7cbeb884df5b87f
Martin Skowronski <skowronski.martin@gmail.com>
2017-05-16 13:24:06 +0200

    added signup and restyled login page

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -8,20 +8,19 @@ var debug = Ember.Logger.log
 export default Ember.Controller.extend({
   ajax: Ember.inject.service(),
   providers: [
-    {"name": "Oddysee",  "link": "auth/oddysee",   "icon": "",         "img": "https://www.oddysee.org/wp-content/plugins/orgtool-wordpress-plugin/orgtool/dist/oddysee-logo-glow.png"},
-    {"name": "Google",   "link": "auth/google",    "icon": "google",   "img": null},
-    {"name": "Github",   "link": "auth/github",    "icon": "github",   "img": null},
-    {"name": "Facebook", "link": "auth/facebook",  "icon": "facebook", "img": null},
-    {"name": "Slack",    "link": "auth/slack",     "icon": "slack",    "img": null}
+//     {"name": "Oddysee",  "link": "auth/oddysee",   "icon": "",         "img": "https://www.oddysee.org/wp-content/plugins/orgtool-wordpress-plugin/orgtool/dist/oddysee-logo-glow.png"},
+    {"name": "Google",   "link": "auth/google",    "icon": "google-plus-square", "img": null},
+    {"name": "Github",   "link": "auth/github",    "icon": "github-square",      "img": null},
+    {"name": "Facebook", "link": "auth/facebook",  "icon": "facebook-square",    "img": null},
+    {"name": "Slack",    "link": "auth/slack",     "icon": "slack",              "img": null}
   ],
   cred: { email: "", password: "", _csrf_token: window.csrf },
+  
 
-  actions: {
-    login: function() {
-//       debug("login", get(this, "cred"));
+  sendRequest: function(data) {
       var prom = this.get('ajax').request('/auth/identity/callback', {
         method: 'POST',
-        data: get(this, "cred")
+        data: data
       });
 
       var self = this;
@@ -33,34 +32,29 @@ export default Ember.Controller.extend({
         console.debug("login err". err);
         window.location.href="/";
       });
+  },
 
+  actions: {
+    setSignup: function() {
+      set(this, "sign", { name: "", email: "", password: "", password_confirmation: "", _csrf_token: window.csrf });
+    },
+
+    unsetSignup: function() {
+      set(this, "sign", null);
+    },
+
+    register: function() {
+      this.sendRequest(get(this, "sign"));
+    },
+
+    login: function() {
+      this.sendRequest(get(this, "cred"));
     },
 
     loginProvider: function(provider) {
       debug("loginProvider", provider);
       window.location.href=provider;
       return;
-
-
-//       if (provider == "google") {
-//       } else if (provider == "google") {
-//         debug("google");
-//       }
     }
   }
-
-
-//   store: Ember.inject.service(),
-//   model: []
-
-//   columns: [20, 20, 20, 20, 20],
-//   itemHeight: 500,
-//   itemWidth: 400,
-//   itemSizes: { width: 400, height: 500},
-//   itemHeight
-//   filteredShips: Ember.computed.filterBy('content', 'player', ),
-//   setup: Ember.on('init', function() {
-//     this.set('content', this.store.findAll('shipCollection'));
-//     this.set('ships', this.store.findAll('ship'));
-//   }),
 });
